d0d3837e8bc2924198d8b5c334d7fcd5d30951ce
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-29 18:25:44 +0100

    dropwdown details now working

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-      <select name="country" id="country" v-model="selectedCountry">
-          <option v-for="(country, index) in countries" :country="country" :key="index" v-bind:value="country"></option>
+      <select name="country" id="country" v-on:change="handleChange" v-model="selectedCountry" >
+          <option v-for="(country, index) in countries" :country="country" :key="index" v-bind:value="country">{{ country.name }}</option>
       </select>
   </div>
 </template>
 
 <script>
+import { eventBus } from '@/main.js';
 export default {
     name: 'country-select',
+    data() {
+        return {
+            selectedCountry: null
+        }
+    },
     props: ['countries'],
     methods: {
         handleChange() {
-            eventbus.$emit('country-selected', this.country)
+            eventBus.$emit('country-selected', this.selectedCountry)
         }
     }
 }
