c38abf1f235bd9ce0b9290a96364301d42533981
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-29 15:49:27 +0100

    added countryDetails

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if="country">
+        <h2>Name: {{ country.name }} </h2>
+        <p>Capital: {{ country.capital }} </p>
+        <p>Population: {{ country.population }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'country-detail',
+    props: ['country']
+}
+</script>
+
+<style>
+
+</style>
